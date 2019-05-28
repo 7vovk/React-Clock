65b959eeeed6bdc1b1544ec864a54d3d8501875e
7vovk <7vovk@rambler.ru>
2019-05-28 14:19:06 +0300

    add working clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            hours: new Date().getHours(),
+            minutes: new Date().getMinutes(),
+            seconds: new Date().getSeconds()
+        };
+    }
+
+    render() {
+        setInterval(() => {
+            this.componentWillMount();
+        }, 1000);
+        return (
+            <div className="App">
+                {this.state.hours} : {this.state.minutes} : {this.state.seconds}
+            </div>
+        )
+    }
+
+    componentWillMount() {
+        if ( this.state.seconds === 0 || this.state.seconds < 10 ) {
+            this.setState({seconds: "0" + new Date().getSeconds()});
+        } else {
+            this.setState({seconds: new Date().getSeconds()});
+        }
+
+        if (this.state.minutes === 0 || this.state.minutes < 10) {
+            this.setState({minutes: "0" + new Date().getMinutes()});
+        } else {
+            this.setState({minutes: new Date().getMinutes()});
+        }
+
+        if (this.state.hours === 0 || this.state.hours < 10) {
+            this.setState({hours: "0" + new Date().getHours()});
+        } else {
+            this.setState({hours: new Date().getHours()});
+        }
+    }
+
+    componentWillUnmount() {
+
+    }
 }
 
 export default App;
